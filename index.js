882ddec74d99f9cef1651bdeebbfecc667445500
Kevin B Owens <owens.bradyn1999@gmail.com>
2021-02-07 17:28:32 -0700

    changing computers

--- index.js
+++ index.js
@@ -112,3 +112,80 @@ function drawSelect() {
 			.text(d => d)
 
 }
+
+function msgToNumber() {
+	var msg = document.getElementById("messagebox").value
+	var n = parseInt(document.getElementById("nresult").innerHTML)
+	console.log(n)
+	if (isNaN(n)) {
+		document.getElementById("messagenumber").innerHTML = "you need to finish the first step!"
+		return
+	}
+	console.log(msg)
+	var number = 0
+	for (var i = 0; i < msg.length; i++){
+		number += msg.charCodeAt(i)
+	}
+	number = number % n
+	console.log(number);
+	document.getElementById("messagenumber").innerHTML = number
+	drawCircleWithMessage()
+}
+
+function drawCircleWithMessage() {
+	width = 50
+	height = 100
+	radius = width / 1.67
+	p = document.getElementById("pinput").value
+	q = document.getElementById("qinput").value
+	n = p * q
+	fields = [{maxValue: n, interval: n/12>>0}]
+
+	d3.selectAll("#pubkeywheel2 > *").remove()
+
+	const svg = d3.select("#pubkeywheel2").append("svg")
+	.attr("viewBox", [0, 0, 100, 100])
+	.attr("text-anchor", "middle")
+
+	const field = svg.append("g")
+	.attr("transform", `translate(${width},${height/4})`)
+	.selectAll("g")
+	.data(fields)
+	.join("g");
+
+	field.append("circle")
+	.attr("fill", "none")
+	.attr("stroke", "currentColor")
+	.attr("stroke-width", .5)
+	.attr("r", 18);
+
+	//What you need: the highest number,
+	// the intervals between, and then a list of each interval
+	//up to the highest, which is returned in that first call to data.
+
+	const fieldTick = field.selectAll("g")
+	.data(d => {
+	  var list = []
+	  for (var i = d.interval; i <= d.maxValue; i+=d.interval) {
+	    list.push({number: i, max: d.maxValue})
+
+	  }
+	  if (!list.includes(n)) {
+	  	list.push({number: n, max: d.maxValue})
+	  	console.log(n)
+	  }
+	  return list
+	})
+	.join("g")
+	  .attr("class", "field-tick")
+	  .attr("transform", (d, i) => {
+	    const angle = d.number / d.max * 2 * Math.PI - Math.PI / 2;
+	    return `translate(${Math.cos(angle) * 20}, ${Math.sin(angle) * 20})`;
+	  }); 
+
+	fieldTick.append("text")
+	.attr("dy", "0.35em")
+	.attr("fill", "#222")
+	.attr("font-size", "2")
+	.text(d => d.number);
+}
